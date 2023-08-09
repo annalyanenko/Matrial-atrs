818d9a52b43c48b1598ccf6a3020a2aa06495f45
annalyanenko <ann.lyanenko18@gmail.com>
2023-08-10 01:34:15 +0300

    JS gallery

--- gallery.js
+++ gallery.js
@@ -1,4 +1,4 @@
-const ACTIVE_SLIDE_CLASSNAME = 'gallery__slide_active';
+const ACTIVE_SLIDE_CLASSNAME = 'gallery__slide-active';
 
 const slidesNodes = Array.from(document.querySelectorAll('.gallery__slide'));
 console.log(slidesNodes);
